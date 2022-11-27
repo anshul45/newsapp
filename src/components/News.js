a644a38b14a20faa82f140eaf4f73846a30c2aa5
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-11-27 13:07:33 +0530

    getting data from news api through fetch api and populating using .map()

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,39 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=1feb78f3400b4e9e896d96369f699020";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({ articles: parseData.articles });
+  }
   render() {
     return (
-      <div>
-        <div>News</div>
-        <NewsItem />
+      <div className="container my-3">
+        <h1>News</h1>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col md-4" key={element.url}>
+                <NewsItem
+                  title={element.title}
+                  description={element.description}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,33 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    return <div>NewsItem</div>;
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              !imageUrl
+                ? "https://www.orissapost.com/wp-content/uploads/2022/11/NASA-Hubble-Telescope-galaxy-.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

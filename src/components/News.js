ffae6815687c226ecf336fad0a2b768376a5f91b
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-11-28 21:42:38 +0530

    adding page to iterate all data from api

--- src/components/News.js
+++ src/components/News.js
@@ -11,12 +11,38 @@ export class News extends Component {
   }
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=1feb78f3400b4e9e896d96369f699020";
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=1feb78f3400b4e9e896d96369f699020&page=1";
     let data = await fetch(url);
     let parseData = await data.json();
     console.log(parseData);
     this.setState({ articles: parseData.articles });
   }
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=1feb78f3400b4e9e896d96369f699020&page=1${
+      this.state.page - 1
+    }`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parseData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=1feb78f3400b4e9e896d96369f699020&page=1${
+      this.state.page + 1
+    }`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+
+    this.setState({
+      page: this.state.page + 1,
+      articles: parseData.articles,
+    });
+  };
   render() {
     return (
       <div className="container my-3">
@@ -34,6 +60,18 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            className="btn btn-dark "
+            onClick={this.handlePreviousClick}
+          >
+            &larr; Previous
+          </button>
+          <button className="btn btn-dark " onClick={this.handleNextClick}>
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
